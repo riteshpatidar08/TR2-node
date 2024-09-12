d769eacead7330b617fa61626a7107d7c031a012
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-09-12 10:11:37 +0530

    fdaf

---
+++ models/productModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose') ;
+
+const productSchema = new mongoose.Schema({
+ name : {
+    type : String,
+    required : true
+ },
+ price : {
+    type : Number,
+    required : true
+ },
+ category : {
+    type : String,
+    required : true
+ }
+
+})
+
+const Product = mongoose.model('Product',productSchema) ;
+
+
+
+
+//name , price , category
